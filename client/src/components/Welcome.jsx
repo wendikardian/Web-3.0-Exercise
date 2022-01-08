b426307cfbe85667e32992117c1fc78a1b9a21b0
wendikardian <wendikardian@gmail.com>
2022-01-08 15:26:49 +0700

    Welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,19 @@
+import { AiFillPlaySquare } from "react-icons/ai";
+import {SiEthereum} from "react-icons/si";
+import {BsInfoCircle} from "react-icons/bs";
+
+import {Loader} from './';
+
 const Welcome = () => {
     return (
-        <h1>
-            This is Welcome
-        </h1>
+        <div className="flex w-full justify-center items-center">
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start  flex-col md:mr-10"></div>
+                <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+                Send Crypto <br/> Across the world 
+                </h1>
+            </div>
+        </div>
     )
 }
 
